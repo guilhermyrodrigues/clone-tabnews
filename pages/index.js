eedefbcbb66c307e6f598bbbd3c811cbf0e464fe
Guilhermy Rodrigues Gomes <121644257+guilhermyrodrigues@users.noreply.github.com>
2025-11-12 00:30:46 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 function Home() {
-    return (
-        <div>
-            <h2>Me segue no <a href="https://github.com/guilhermyrodrigues">GITHUB</a></h2>
-        </div>
-        
-    )
+  return (
+    <div>
+      <h2>
+        Me segue no <a href="https://github.com/guilhermyrodrigues">GITHUB</a>
+      </h2>
+    </div>
+  );
 }
 
 export default Home;
