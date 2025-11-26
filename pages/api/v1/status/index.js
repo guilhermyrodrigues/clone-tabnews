a489f439eb669a44a1289fff5ce05ca26272946b
Guilhermy Rodrigues <guilhermyr87@gmail.com>
2025-11-25 22:05:08 -0300

    add a more robust ssl configuration method

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -14,7 +14,6 @@ async function status(req, res) {
     databaseMaxConnectionResult.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
-
   const databaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
