d1add39a8e4e539d410dc215fe160f50bfb4c01d
Guilhermy Rodrigues Gomes <121644257+guilhermyrodrigues@users.noreply.github.com>
2026-03-26 23:57:08 +0000

    feat: implement session creation

--- infra/controller.js
+++ infra/controller.js
@@ -3,6 +3,7 @@ import {
   MethodNotAllowedError,
   ValidationError,
   NotFoundError,
+  UnauthorizedError,
 } from "infra/errors";
 
 function onNoMatchHandler(req, res) {
@@ -11,12 +12,15 @@ function onNoMatchHandler(req, res) {
 }
 
 function onErrorHandler(err, req, res) {
-  if (err instanceof ValidationError || err instanceof NotFoundError) {
+  if (
+    err instanceof ValidationError ||
+    err instanceof NotFoundError ||
+    err instanceof UnauthorizedError
+  ) {
     return res.status(err.statusCode).json(err);
   }
 
   const publicErrorObject = new InternalServerError({
-    statusCode: err.statusCode,
     cause: err,
   });
 
